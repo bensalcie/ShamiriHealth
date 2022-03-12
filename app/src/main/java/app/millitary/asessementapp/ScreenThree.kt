f75c923c2389c6b47e468c61e82cbea70e56f942
bensalcie <bensalcie@gmail.com>
2022-03-13 02:21:07 +0300

    Initial commit

---
+++ app/src/main/java/app/millitary/asessementapp/ScreenThree.kt
@@ -0,0 +1,11 @@
+package app.millitary.asessementapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScreenThree : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_three)
+    }
+}
